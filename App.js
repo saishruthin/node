9f881c8efd0f04837f6381d8ac594996ed4cd343
saishruthin <145509040+saishruthin@users.noreply.github.com>
2024-12-10 20:32:37 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Header from './components/header';
+import Footer from './components/footer';
+import Counter from './components/counter' 
+
+function App() {
+    return (
+        <div className = "App">
+            <Header />
+            <main>
+                <h2>About us</h2>
+                <p>And this is a simple fucking website and please stick to something sai shruthin kenche fuck man</p>
+                <h3>And this is a fucking counter</h3>
+                <counter />
+            </main>
+            <Footer />
+
+
+        </div>
+
+
+    );
+}
+export default App;
